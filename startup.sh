a7e656abb34ac522cff2c9decda244f6fe7ab6b8
Athfan Khaleel <athphane@gmail.com>
2023-09-14 21:17:08 +0500

    update dockerfile

--- startup.sh
+++ startup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-function get_property
-{
-    sed "/^$2 *= */!d; s///" "$1"
+
+function get_property {
+    sed -n "s/^$2 *= *//p" "$1"
 }
-NAME=`get_property ./server-settings.ini serverName`
-PASSWORD=`get_property ./server-settings.ini connectPassword`
-IP=`get_property ./server-settings.ini connectip`
-PORT=`get_property ./server-settings.ini connectPort`
 
-#printf "$NAME\n$PASSWORD\n$IP\n$PORT" | cloneheroserver
+NAME=$(get_property ./server-settings.ini serverName)
+PASSWORD=$(get_property ./server-settings.ini connectPassword)
+IP=$(get_property ./server-settings.ini connectip)
+PORT=$(get_property ./server-settings.ini connectPort)
 
+if [ -z "$NAME" ] || [ -z "$PASSWORD" ] || [ -z "$IP" ] || [ -z "$PORT" ]; then
+    echo "One or more properties not found in server-settings.ini"
+    exit 1
+fi
+
+# Assuming cloneheroserver is in your PATH, you can directly run it
 cloneheroserver -n "$NAME" -d -ps "$PASSWORD"
